f2601d26fc89f9310fa9c99ec341468ebd59965d
mechanoid7 <mechanis1@gmail.com>
2021-11-13 00:05:07 +0200

    Clean up code

--- src/main/java/com/example/persimmoncocktails/controllers/PersonController.java
+++ src/main/java/com/example/persimmoncocktails/controllers/PersonController.java
@@ -1,6 +1,6 @@
 package com.example.persimmoncocktails.controllers;
 
-import com.example.persimmoncocktails.dao.impl.PersonDaoImpl;
+import com.example.persimmoncocktails.dao.PersonDao;
 import com.example.persimmoncocktails.models.Person;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,57 +12,62 @@ import java.util.List;
 @RequestMapping("/person")
 public class PersonController {
 
-    private final PersonDaoImpl personDaoImpl;
+    private final PersonDao personDao;
 
-    public PersonController(PersonDaoImpl personDaoImpl) {
-        this.personDaoImpl = personDaoImpl;
+    public PersonController(PersonDao personDao) {
+        this.personDao = personDao;
     }
 
     @PostMapping("/add")
-    private HttpStatus addPerson(@RequestParam String name, @RequestParam String  email, @RequestParam String password,
-                           @RequestParam Long photoId, @RequestParam Long blogId, @RequestParam Integer roleId){
-        personDaoImpl.create(new Person(0L, name, email, password, photoId, blogId, roleId));
-        return HttpStatus.CREATED;
+    private void addPerson(@RequestParam String name, @RequestParam String  email, @RequestParam String password
+            , @RequestParam Integer roleId){
+        personDao.create(
+                Person.builder()
+                        .name(name)
+                        .email(email)
+                        .password(password)
+                        .roleId(roleId)
+                        .build());
     }
 
     @GetMapping("/{personId}")
     public Person getPersonById(@PathVariable Long personId){
-        return personDaoImpl.read(personId);
+        return personDao.read(personId);
     }
 
     @GetMapping("/email={personEmail}")
     private Person getPersonByEmail(@PathVariable String personEmail){
-        return personDaoImpl.read(personEmail);
+        return personDao.read(personEmail);
     }
 
     @PatchMapping("/update")
     private HttpStatus updatePerson(@RequestParam Long personId, @RequestParam String name, @RequestParam String  email,
                               @RequestParam String password, @RequestParam Long photoId, @RequestParam Long blogId,
                               @RequestParam Integer roleId){
-        personDaoImpl.update(new Person(personId, name, email, password, photoId, blogId, roleId));
+        personDao.update(new Person(personId, name, email, password, photoId, blogId, roleId));
         return HttpStatus.OK;
     }
 
     @DeleteMapping("/{personId}")
     private HttpStatus deletePersonById(@PathVariable Long personId){
-        personDaoImpl.delete(personId);
+        personDao.delete(personId);
         return HttpStatus.ACCEPTED;
     }
 
     @PatchMapping("/change-password")
     private HttpStatus changePasswordPerson(@RequestParam Long personId, @RequestParam String oldPassword,
                                       @RequestParam String  newPassword){
-        personDaoImpl.changePassword(personId, oldPassword, newPassword);
+        personDao.changePassword(personId, oldPassword, newPassword);
         return HttpStatus.OK;
     }
 
     @GetMapping("/{personId}/friends")
     private List<Person> getPersonFriends(@PathVariable Long personId){
-        return personDaoImpl.getPersonFriends(personId);
+        return personDao.getPersonFriends(personId);
     }
 
     @GetMapping("/{personId}/friends/{substring}")
     private List<Person> getPersonFriendsBySubstring(@PathVariable Long personId, @PathVariable String substring){
-        return personDaoImpl.getListFriendBySubstring(personId, "%"+substring+"%");
+        return personDao.getListFriendBySubstring(personId, "%"+substring+"%");
     }
 }
--- src/main/java/com/example/persimmoncocktails/models/Person.java
+++ src/main/java/com/example/persimmoncocktails/models/Person.java
@@ -1,10 +1,12 @@
 package com.example.persimmoncocktails.models;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Person {
