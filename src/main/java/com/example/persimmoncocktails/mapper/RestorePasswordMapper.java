1851c817cfd48944118a5d9b6b36d5a892b33b5c
mechanoid7 <mechanis1@gmail.com>
2021-11-14 13:26:27 +0200

    Fix RestorePasswordMapper

--- src/main/java/com/example/persimmoncocktails/mapper/RestorePasswordMapper.java
+++ src/main/java/com/example/persimmoncocktails/mapper/RestorePasswordMapper.java
@@ -5,13 +5,18 @@ import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
 public class RestorePasswordMapper  implements RowMapper<RestorePasswordDataDto> {
     @Override
     public RestorePasswordDataDto mapRow(ResultSet rs, int rowNum) throws SQLException {
-        LocalDateTime localDateTime = rs.getTimestamp("time").toLocalDateTime();
-        if(rs.wasNull()) localDateTime = null;
+        Timestamp timestamp = rs.getTimestamp("time");
+        LocalDateTime localDateTime;
+        if(rs.wasNull()) {
+            localDateTime = null;
+        } else localDateTime = timestamp.toLocalDateTime();
+
         return new RestorePasswordDataDto(rs.getLong("person_id"), localDateTime, rs.getString("id"));
     }
 }
