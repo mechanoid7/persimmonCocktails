b948b021fd1e8250e03f83c582b595b4aaf5c673
mechanoid7 <55248870+mechanoid7@users.noreply.github.com>
2021-11-15 16:33:22 +0200

    Revert "Add dtos for any RequestBody"

--- src/main/java/com/example/persimmoncocktails/PersimmonCocktailsApplication.java
+++ src/main/java/com/example/persimmoncocktails/PersimmonCocktailsApplication.java
@@ -2,8 +2,6 @@ package com.example.persimmoncocktails;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class PersimmonCocktailsApplication {
@@ -12,9 +10,4 @@ public class PersimmonCocktailsApplication {
         SpringApplication.run(PersimmonCocktailsApplication.class, args);
     }
 
-
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder(){
-        return new BCryptPasswordEncoder(9);
-    }
 }
--- src/main/java/com/example/persimmoncocktails/dao/ModeratorDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.persimmoncocktails.dao;
-
-import com.example.persimmoncocktails.models.Person;
-
-import java.util.List;
-
-public interface ModeratorDao {
-    List<Person> getAllModerators();
-}
