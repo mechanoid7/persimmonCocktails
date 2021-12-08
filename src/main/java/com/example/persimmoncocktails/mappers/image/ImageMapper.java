a76456ea47b00bee54909f8f1b6d8e6517a58af2
mechanoid7 <mechanis1@gmail.com>
2021-12-08 21:54:46 +0200

    Fix check imageId nullable

--- src/main/java/com/example/persimmoncocktails/mappers/image/ImageMapper.java
+++ src/main/java/com/example/persimmoncocktails/mappers/image/ImageMapper.java
@@ -9,14 +9,12 @@ import java.sql.SQLException;
 public class ImageMapper implements RowMapper<ImageResponseDto> {
     @Override
     public ImageResponseDto mapRow(ResultSet rs, int rowNum) throws SQLException {
-        Long imageId = rs.getLong("photo_id");
-        if (rs.wasNull()) imageId = null;
         String urlFull = rs.getString("url_full");
         if (rs.wasNull()) urlFull = null;
         String urlMiddle = rs.getString("url_middle");
         if (rs.wasNull()) urlMiddle = null;
         String urlThumb = rs.getString("url_thumb");
         if (rs.wasNull()) urlThumb = null;
-        return new ImageResponseDto(imageId, urlFull, urlMiddle, urlThumb);
+        return new ImageResponseDto(rs.getLong("photo_id"), urlFull, urlMiddle, urlThumb);
     }
 }
